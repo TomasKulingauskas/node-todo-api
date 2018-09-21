7c9eca6b139013b5039e649087b77755c33e9ccc
Tomas <tomaskulingauskas@gmail.com>
2018-09-21 23:42:43 +0300

    add update script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -8,26 +8,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
 	console.log('Connected to mongodb server');
 	const db = client.db('TodoApp');
 
-	// db.collection('Todos').insertOne({
-	// 	text: 'Something to do',
-	// 	completed: false
-	// }, (err, result) => {
-	// 	if (err) {
-	// 		return console.log('Unable to insert todo', err);
-	// 	}
-	// 	console.log(JSON.stringify(result.ops, undefined, 2))
-	// })
+	db.collection('Todos').insertOne({
+		text: 'Something to do',
+		completed: false
+	}, (err, result) => {
+		if (err) {
+			return console.log('Unable to insert todo', err);
+		}
+		console.log(JSON.stringify(result.ops, undefined, 2))
+	})
 
-	// db.collection('Users').insertOne({
-	// 	name: 'tomask',
-	// 	age: 24,
-	// 	location: 'Kaunas, LT'
-	// }, (err, result) => {
-	// 	if (err) {
-	// 		return console.log('Unable to insert user', err);
-	// 	}
-	// 	console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2))
-	// })
+	db.collection('Users').insertOne({
+		name: 'tomask',
+		age: 24,
+		location: 'Kaunas, LT'
+	}, (err, result) => {
+		if (err) {
+			return console.log('Unable to insert user', err);
+		}
+		console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2))
+	})
 
 	client.close();
 });
